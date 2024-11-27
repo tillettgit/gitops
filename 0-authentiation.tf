ae095ce7525be1ade62b7cbfa814043d8b277a00
tillettgit <tillettc@hotmail.com>
2024-11-27 11:17:19 +0800

    let's try this again

--- 0-authentiation.tf
+++ 0-authentiation.tf
@@ -1,1 +1,15 @@
-#Authentication file
+# provider
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
